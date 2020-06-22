1072e6c0bb2c03bde5def8daf5b41b268664d0a6
Linh Tran <linh.tran@skedulo.com>
2020-06-22 14:52:41 +0700

    seperate component

--- projects/src/pages/ScheduleTab/generateScheduleCell.tsx
+++ projects/src/pages/ScheduleTab/generateScheduleCell.tsx
@@ -5,78 +5,19 @@ import { format } from 'date-fns-tz'
 import { getDaysInMonth, eachDayOfInterval, add, isToday } from 'date-fns'
 import { RangeType, Avatar, GroupAvatars, Tooltip } from '@skedulo/sked-ui'
 import { IJobDetail, ITimeOption, ISwimlaneSettings, IWorkingHours } from '../../commons/types'
+import ScheduledJob from './ScheduledJob'
 import {
   SCHEDULE_JOB_STATUS_COLOR,
   DATE_FORMAT,
 } from '../../commons/constants'
 import {
   getTimePickerOptions,
-  parseDurationValue,
   parseTimeValue,
   parseDurationFromTimeValueRange
 } from '../../commons/utils'
 
 const DEFAULT_SLOT_WIDTH = 50
 const SLOT_WIDTH_UNIT = 'px'
-
-interface IScheduledJobProps {
-  job: IJobDetail
-  jobPosition: string
-  travelTimeStyle: Record<string, string>
-  durationStyle: Record<string, string>
-  travelTime: number
-}
-
-const ScheduledJob: React.FC<IScheduledJobProps> = ({
-  job,
-  jobPosition,
-  travelTime,
-  travelTimeStyle,
-  durationStyle
-}) => {
-  const resources: any = []
-  const time = Math.max(job.allocations?.length || 0, toNumber(job.resourceRequirement))
-  times(index => {
-    const jobAllocation = job.allocations ? job.allocations[index] : null
-
-    resources.push(
-      <Avatar
-        name={jobAllocation?.resource?.name || ''}
-        key={`resourcerquired-${index}`}
-        className={classnames('cx-ml-1 first:cx-ml-0', {
-          'cx-bg-blue-100 cx-border cx-border-dotted cx-border-blue-500': !jobAllocation
-        })}
-        showTooltip={!!jobAllocation?.resource?.name}
-        size="small"
-        preserveName={false}
-      />
-    )
-  }, time > 0 ? time : 1)
-
-  return (
-    <div className="cx-flex cx-items-center cx-absolute cx-z-1" style={{
-      height: '80%',
-      left: jobPosition,
-    }}>
-      {travelTime > 0 && (
-        <span className="cx-bg-neutral-500" style={travelTimeStyle} />
-      )}
-      <span className="cx-h-full" style={durationStyle} />
-      <span className="cx-flex">
-        {resources}
-        {/* <GroupAvatars
-          totalSlots={time > 0 ? time : 1}
-          maxVisibleSlots={5}
-          avatarInfo={resources}
-          size="medium"
-        /> */}
-      </span>
-      <span className="cx-pl-2" style={{ width: 'max-content' }}>
-        {parseDurationValue(job.duration)}
-      </span>
-    </div>
-  )
-}
 
 const generateScheduleDataCell = (
   rangeType: RangeType,
